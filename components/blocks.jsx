c8c8b662f2efde51d6a149908125852a770ad152
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-08 21:14:54 +0500

    Update blocks.jsx
    надо что-то с рендером доделать

--- components/blocks.jsx
+++ components/blocks.jsx
@@ -1,25 +1,56 @@
 import React, {Component} from 'react';
-import Block from './block';
+// import Block from './block';
+
+var initialState = {
+    blockS:[
+        {
+            title:'commerce',
+            text:'ldiuf hsdk fhosdf jsdof pjoify'
+        },
+        {
+            title:'offers',
+            text:'oqwer tyui oasd fghjk'
+        },
+        {
+            title:'products',
+            text:'nbvcxz asd fghjk lpoi uytrew'
+        }
+    ]
+}
+
+
 
 export default class Blocks extends Component {
     render(){
         return(
-            <div className="container container__first">
-                <div className="blocks">
-                <div className="row">
-                    <div className="col-sm">
-                        <Block  />
-                    </div>
-                    <div className="col-sm">
-                        <Block />
-                    </div>
-                    <div className="col-sm">
-                        <Block />
-                    </div>
-                </div>
-                </div>
+            <div  className="container container__first">
+                <div id="blockList"></div>
+                ${
+                var Blockk = document.getElementById('blockList')
+                function renderText(){
+                    let innerContent = ''
+                    initialState.blockS.map((orderObj, id) => {
+                        innerContent += `
+                        <div className="block_line col-sm"">
+                            <div className="block__one">
+                                <div className="block__foto"></div>
+                                <div className="block__title">
+                                    ${orderObj.title}
+                                </div>
+                                <div className="block__text">
+                                    ${ orderObj.text}
+                                </div>
+                            </div>
+                        </div>
+                        `
+                        })
+                    Blockk.html(innerContent);
+                    }
+                renderText();
+                }
             </div>
 
         )
     }
 }
+
