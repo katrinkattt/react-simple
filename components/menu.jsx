bc9dbf9605330453ff63137973b7d6649cfe236f
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-08 17:23:24 +0500

    Create menu.jsx

---
+++ components/menu.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Wallpaper  from './wallpaper';
+
+export default class Header extends Component {
+    render(){
+        return(
+            <div className="main__page">
+            <header>
+                <nav className="navbar navbar-expand-lg  nav_bar">
+                    <a className="navbar-brand" href="#">Menu</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse nav_center" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ">
+                            <a className="nav-item nav-link active" href="./main.html"> Home<span class="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link " href="#">Gallery</a>
+                            <a className="nav-item nav-link" href="#">Links</a>
+                            <a className="nav-item nav-link" href="#">About</a>
+                        </div>
+                    </div>
+                </nav>
+        </header>
+        <div className="conteiner">
+            <Wallpaper  />
+        </div>
+        </div>
+        )
+    }
+}
