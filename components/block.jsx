621dfce2f250bf98a43e409ce364b79a2060a24c
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-08 17:26:18 +0500

    Create block.jsx
    настроить рендер из массива

---
+++ components/block.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+
+const words = [
+    {id:1, title:'text1', content:'slovaaa mnogo slov'},
+    {id:2, title:'text2', content:'eche slova i eche'},
+    {id:3, title:'text3', content:'v thret zagolovke'}
+];
+
+function RenderText(props){
+       // ADD FOR HZ
+        const texts = (
+        <div>
+            {props.words.map((post) =>
+            <p key={post.id}>
+                {post.title}
+            </p>
+            )}
+        </div>
+    )
+    const content = (
+    <div>
+        {props.words.map((post) =>
+            <p key={post.id}>
+                {post.content}
+            </p>
+        )}
+    </div>
+    );
+    return(
+        <div>
+            {texts}
+            <hr />
+            {content}
+        </div>
+    );
+}
+
+export default class Block extends Component {
+    render(){
+        return(
+            <div className="block_line">
+                <div className="block__one">
+                    <div className="block__foto"></div>
+                    <div className="block__text">
+                        <RenderText words={words} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
