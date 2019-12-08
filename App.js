609281a2f83ff1fa729850367b50b72de925c9d0
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-08 20:36:48 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/menu.jsx';
+import Blocks from './components/blocks.jsx';
+import Form from './components/form';
+import Footer from './components/footer';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <section>
+        <Header />
+      </section>
+      <section>
+        <Blocks />
+      </section>
+      <section>
+        <Form />
+      </section>
+      <section>
+        <Footer />
+      </section>
+    </div>
+  );
+}
+
+export default App;
