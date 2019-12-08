48e3cfc1cc0a1b36139a59080fa31658619cb0e6
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-08 18:40:37 +0500

    Create footer.jsx

---
+++ components/footer.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Footer extends Component{
+    render(){
+        return(
+            <footer>
+                <div className='footer__text'>
+                    <p>2019</p>
+                </div>
+            </footer>
+        )
+    }
+}
