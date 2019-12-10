fa78fe2638b17d124a88cbe9535ba02b2e221250
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-10 19:07:39 +0500

    Update form.jsx

--- components/form.jsx
+++ components/form.jsx
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import FormBlock from './formBlock';
 import BtnForm from './btn';
-import '../'
+
 // import { placeholder, labeledStatement } from '@babel/types';
 
 export default class Form extends Component {
     render(){
         return(
             <div className="container container__form">
-                <div className="title__main">Feedback</div>
+                <div className="title__section">Feedback</div>
                 <div className="block__main__form">
                     <FormBlock />
                     <FormBlock />
