54daf4a349cae31dbc566eeef767abdff6396c37
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-23 18:01:36 +0500

    reorganization

--- components/blocks.jsx
+++ components/blocks.jsx
@@ -5,21 +5,36 @@ import Block from './block';
 export default class Blocks extends Component {
     initialState = {
         currentId:0,
-        blockS:[
-            {
-                title:'commerce',
-                text:'ldiuf hsdk fhosdf jsdof pjoify'
-            },
-            {
-                title:'offers',
-                text:'oqwer tyui oasd fghjk'
-            },
-            {
-                title:'products',
-                text:'nbvcxz asd fghjk lpoi uytrew'
-            }
-        ]
-    }
+        title:[
+                'commerce',
+                'offers',
+                'products',
+            ],
+        text:[
+            'ldiuf hsdk fhosdf jsdof pjoify',
+            'oqwer tyui oasd fghjk',
+            'nbvcxz asd fghjk lpoi uytrew'
+            ]
+            
+            
+    }       
+            
+//         blockS:[
+            
+//             {
+//                 title:'commerce',
+//                 text:'ldiuf hsdk fhosdf jsdof pjoify'
+//             },
+//             {
+//                 title:'offers',
+//                 text:'oqwer tyui oasd fghjk'
+//             },
+//             {
+//                 title:'products',
+//                 text:'nbvcxz asd fghjk lpoi uytrew'
+//             }
+//         ]
+//     }
     
     render(){
         return(
@@ -27,14 +42,15 @@ export default class Blocks extends Component {
                 <div className="title__section">Offers</div>
                 <div className="blockLine col">
                 <div class="row align-items-center">
+                    this.initialState.
                     <div class="col">
-                        <Block title={this.initialState.blockS[0].title} text={this.initialState.blockS[0].text} />
+                        <Block title={this.initialState.title} text={this.initialState.text} />
                     </div>
                     <div class="col">
-                        <Block title={this.initialState.blockS[1].title} text={this.initialState.blockS[1]}/>
+                        <Block title={this.initialState.title} text={this.initialState.text]}/>
                     </div>
                     <div class="col">
-                        <Block title={this.initialState.blockS[2].title} text={this.initialState.blockS[2]} />
+                        <Block title={this.initialState.title} text={this.initialState.text} />
                     </div>
                 </div>
                 </div>
